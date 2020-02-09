50032d5957b94fd27567d29a7eb00dc7e78d4c48
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-02-09 23:39:30 +0200

    Add ElementBuilderInterfaceDrawer

---
+++ Assets/UGF.Elements.Runtime.Tests/TestElementBuilderFields.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace UGF.Elements.Runtime.Tests
+{
+    public class TestElementBuilderFields : MonoBehaviour
+    {
+        [SerializeReference] private IElementBuilder m_builder1;
+        [SerializeField] private IElementBuilder m_builder2;
+    }
+}
