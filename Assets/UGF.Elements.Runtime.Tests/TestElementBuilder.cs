97319e93f9cb62068b9fc08b9dc2da6166db629d
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-02-11 23:08:38 +0200

    Add test

--- Assets/UGF.Elements.Runtime.Tests/TestElementBuilder.cs
+++ Assets/UGF.Elements.Runtime.Tests/TestElementBuilder.cs
@@ -1,7 +1,18 @@
-﻿namespace UGF.Elements.Runtime.Tests
+﻿using UnityEngine;
+
+namespace UGF.Elements.Runtime.Tests
 {
     public class TestElementBuilder : ElementBuilder
     {
+        private void Start()
+        {
+            IElement element = Build(new ElementContext());
+
+            element.Initialize();
+
+            Debug.Log(element.Children.Count);
+        }
+
         protected override IElement OnBuild(IElementContext context)
         {
             return new Element();
