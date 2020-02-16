508a2692fe03f5842aaf241d0ed95cc872b2c174
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-02-16 20:19:23 +0200

    Add ElementParentBuilderEditor

---
+++ Packages/UGF.Elements/Editor/ElementParentBuilderAssetEditor.cs
@@ -0,0 +1,10 @@
+﻿using UGF.Elements.Runtime;
+using UnityEditor;
+
+namespace UGF.Elements.Editor
+{
+    [CustomEditor(typeof(ElementParentBuilderAsset), true)]
+    internal class ElementParentBuilderAssetEditor : ElementParentBuilderEditor
+    {
+    }
+}
